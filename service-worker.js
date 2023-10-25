5bc3aad7db267a2327ea076fdf869af120aa07af
hollerith <hollerith@users.noreply.github.com>
2023-10-25 21:57:27 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+// Files to cache
+const cacheName = 'myCache-v1';
+const cacheAssets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/newt.min.js'
+  '/main.js'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Installed');
+  event.waitUntil(
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        console.log('Service Worker: Caching Files');
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Activated');
+  // Remove old caches
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            console.log('Service Worker: Clearing Old Cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  console.log('Service Worker: Fetching');
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
